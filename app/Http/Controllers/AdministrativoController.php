d406b8e1b18f650ed26f56bc2a48e49aa0108233
Jaime Berrazueta <jaimeandres@users.noreply.github.com>
2018-12-20 16:11:22 -0500

    carga inventario

--- app/Http/Controllers/AdministrativoController.php
+++ app/Http/Controllers/AdministrativoController.php
@@ -32,6 +32,32 @@ class AdministrativoController extends Controller
                 $fileName = $file->getClientOriginalName();
                 $file->move($path, $fileName);
             }
+
+
+
+
+        $input = Input::all();
+ 
+        $rules = array(
+            'file' => 'image|max:3000',
+        );
+ 
+        $validation = Validator::make($input, $rules);
+ 
+        if ($validation->fails()) {
+            return Response::make($validation->errors->first(), 400);
+        }
+ 
+        $destinationPath = 'uploads'; // upload path
+        $extension = Input::file('file')->getClientOriginalExtension(); // getting file extension
+        $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
+        $upload_success = Input::file('file')->move($destinationPath, $fileName); // uploading file to given path
+ 
+        if ($upload_success) {
+            return Response::json('success', 200);
+        } else {
+            return Response::json('error', 400);
+        }
 	}
 
 	public function show()
--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -33,5 +33,32 @@ class FileController extends Controller
 		$path = "/images/products/";
 		$file->move(public_path().$path, $filename);
 		$product->url_img = $path.$filename;
+
+
+
+
+
+		$input = Input::all();
+ 
+        $rules = array(
+            'file' => 'image|max:3000',
+        );
+ 
+        $validation = Validator::make($input, $rules);
+ 
+        if ($validation->fails()) {
+            return Response::make($validation->errors->first(), 400);
+        }
+ 
+        $destinationPath = 'uploads'; // upload path
+        $extension = Input::file('file')->getClientOriginalExtension(); // getting file extension
+        $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
+        $upload_success = Input::file('file')->move($destinationPath, $fileName); // uploading file to given path
+ 
+        if ($upload_success) {
+            return Response::json('success', 200);
+        } else {
+            return Response::json('error', 400);
+        }
 	}
 }
