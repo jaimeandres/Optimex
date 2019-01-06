de76816d77d3608a616a39f568a5f8fecc8aa230
Jaime Berrazueta <jaimeandres@users.noreply.github.com>
2019-01-06 03:29:42 -0500

    validacion campo

--- resources/views/usuario/edit.blade.php
+++ resources/views/usuario/edit.blade.php
@@ -38,21 +38,21 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label">Nombre</label>
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ $usuarios->name }}">
+                                <input type="text" class="form-control" name="name" value="{{ $usuarios->name }}" required>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-md-4 control-label">E-Mail</label>
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ $usuarios->email }}">
+                                <input type="email" class="form-control" name="email" value="{{ $usuarios->email }}" required>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-md-4 control-label">Nueva Contraseña</label>
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password" value="{{ $usuarios->password }}">
+                                <input type="password" class="form-control" name="password" value="{{ $usuarios->password }}" required>
                             </div>
                         </div>
 
